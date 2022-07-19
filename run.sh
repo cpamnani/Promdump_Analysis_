1ebf0b2974a4bfc6471a527fece3f9fa6696fd0c
Chintan Pamnani <cpamnani@redhat.com>
2022-07-19 12:30:47 -0700

    Updating script

--- run.sh
+++ run.sh
@@ -7,7 +7,12 @@ chmod -R g+w $1
 cd ..
 podman run --user $(id -u) --userns keep-id \
              -v ./$1:/prometheus:rw,Z \
-             -p 9090:9090 prom/prometheus
-             
-/Promdump_Analysis/grafana-9.0.2/bin/grafana-serve
+             -p 9090:9090 prom/prometheus &
+mkdir grafana_data
+cd grafana_data
+podman run --name $3 -d -p 3000:3000 -v $(pwd):/grafana_data/db:Z grafana 
+podman run --rm -it grafana &
+
+
+
 	
